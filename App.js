49c7de33c4b05343ef3c9d800d7e5e9a39857238
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-12-19 23:56:20 +0530

    Boilerplate setup

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers';
 
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+    apiKey: "AIzaSyC7gA4iybV_E_B8_TFtiAKBt0pus3sdt-k",
+    authDomain: "manager-7d2ac.firebaseapp.com",
+    databaseURL: "https://manager-7d2ac.firebaseio.com",
+    projectId: "manager-7d2ac",
+    storageBucket: "manager-7d2ac.appspot.com",
+    messagingSenderId: "908679459002"
+  };
+  firebase.initializeApp(config);
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
